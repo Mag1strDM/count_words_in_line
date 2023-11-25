4ecd6b5b72585ed6e8fa86c554cf9336d98f2475
Mag1strDM <110253115+Mag1strDM@users.noreply.github.com>
2023-11-25 12:34:55 +0300

    Add files via upload

--- ConsoleApplication11.cpp
+++ ConsoleApplication11.cpp
@@ -8,38 +8,38 @@ void count__words(string line) {
 	char const z = ' ';
 
 	bool isOnlySpaces = true;
-	for (int j = 0; j < line.size(); j++) { // если в строке есть хоть один элемент отличный от пробела, то выходим из цикла проверка
+	for (int j = 0; j < line.size(); j++) {
 		if (line[j] != z) { 
 			isOnlySpaces = false;
 			break;
 		}
 	}
 	if (isOnlySpaces == true) {
-		cout << "нет слов в строке" << endl; // если в строке только пробелы, то заканчиваем программу
+		cout << "нет слов в строке" << endl;
 		exit(0);
 	}
 
 	if (line[0] != z) {
-		numberOfWords++; // является ли первый элемент символом отличным от пробела
+		numberOfWords++;
 	}
 
 	for (int i = 0; i < line.size(); i++) {
-		if (line[i] == z && line[i + 1] != z) { // если символ равен пробелу и следующий символ не равен пробелу, то прибавляем к сумме 1
-			numberOfWords++;					// надо для проверки, что пробел разделяет два символа
-		}										// если после пробела идет еще один пробел, то мы пропускаем этот элемент и идем дальше
+		if (line[i] == z && line[i + 1] != z) {
+			numberOfWords++;
+		}
 	}
 
 	if (line.back() == z) {
-		cout << numberOfWords - 1<< endl; // если последний элемент строки - пробел, то мы вычитаем из суммы один, чтобы пробел не считался за символ
-		exit(0);						  // выводим сумму и заканчиваем программу
+		cout << numberOfWords - 1<< endl;
+		exit(0);
 	}
 
-	cout << numberOfWords << endl; // выводим сумму символов в строке
+	cout << numberOfWords << endl;
 }
 
 int main() {
 	setlocale(LC_ALL, "Russian");
 	string line;
-	getline(cin, line); //считывем введенные данные и присваем их line
+	getline(cin, line);
 	count__words(line);
 }
